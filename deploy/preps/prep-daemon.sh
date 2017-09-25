3fe033947dc85046b4b01195b62b8a99eb549f09
vagharsh@kandilian.com <vagharsh@users.noreply.github.com>
2017-09-25 12:43:46 +0400

    relative paths fix

--- deploy/preps/prep-daemon.sh
+++ deploy/preps/prep-daemon.sh
@@ -5,4 +5,4 @@ DOCKER_BIP=${1:-10.250.250.1/24}
 mkdir /etc/docker
 chmod 700 /etc/docker
 
-envsubst < "../confs/daemon.json" > "/etc/docker/daemon.json"
+envsubst < "confs/daemon.json" > "/etc/docker/daemon.json"
