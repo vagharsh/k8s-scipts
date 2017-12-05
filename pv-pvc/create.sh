1db7e9c669ba60e7170890adeae330510e9c79d3
Vagharsh Kandilian <vagharsh@users.noreply.github.com>
2017-12-05 18:54:36 +0400

    Add files via upload

---
+++ pv-pvc/create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source envvars.sh
+
+usage() { echo "Please fill the NAMESPACE parameter in envvars.sh file" 1>&2; exit 1; }
+
+if [ -z "$NAMESPACE" ]; then
+  usage
+fi
+
+echo "[INFO] Creating Persistant Volume as $FOR_SVC_NAME-pv-$NAMESPACE"
+envsubst < pv.json | kubectl create -f -
+
+echo "[INFO] Creating Persistant Volume Claim as $FOR_SVC_NAME-pvc"
+envsubst < pvc.json | kubectl create -f -
---
+++ pv-pvc/envvars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+export NAMESPACE="demo"
+export FOR_SVC_NAME="service"
+export STORAGE_SIZE="3Gi"
+export NFS_SERVER="172.20.20.1"
+export NFS_PATH="/shared/dir/service"
