a190a9900fe7bdc1f0c900f3c084e697fd644ca0
Vagharsh Kandilian <vagharsh@users.noreply.github.com>
2017-12-15 07:07:53 +0400

    update docker installation

--- kubernetes/preps/docker-setup.sh
+++ kubernetes/preps/docker-setup.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-cat >/etc/yum.repos.d/docker.repo <<-EOF
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-
-yum install -y docker-engine-1.12.6
+yum install -y docker
 
 key="^exclude.*"
 listOfExcludes=`cat /etc/yum.conf | grep "exclude="`
