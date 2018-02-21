eb11e56a4d1ae1eaabf3d3c3c4d52bd2c044e08b
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2018-02-21 18:24:56 +0400

    kube-version default value

--- kubernetes/envvars.sh
+++ kubernetes/envvars.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_BIP="10.250.250.1/24"
 export BLOCK_DEVICE="/dev/sdb"
-export KUBE_VERSION="v1.6.7"
+export KUBE_VERSION="" # e.g. ( v1.6.7 )
 export KUBE_ADVERTISE_IP="" # The IP address the API Server will advertise it's listening on.
 export NAMESPACE=("demo" "prod")
 export TIMEZONE="Asia/Yerevan"
