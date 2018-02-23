db487df87d2383d91a99cf6c19d049302f165179
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2018-02-23 17:40:11 +0400

    removes extra comments

--- kubernetes/envvars.sh
+++ kubernetes/envvars.sh
@@ -2,8 +2,8 @@
 
 export DOCKER_BIP="10.250.250.1/24"
 export BLOCK_DEVICE="/dev/sdb"
-export KUBE_VERSION="" # e.g. ( v1.6.7 )
-export KUBE_ADVERTISE_IP="" # The IP address the API Server will advertise it's listening on.
+export KUBE_VERSION=""
+export KUBE_ADVERTISE_IP=""
 export NAMESPACE=("demo" "prod")
 export TIMEZONE="Asia/Yerevan"
 export DEFAULT_NIC="eth0"
