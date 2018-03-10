7e4a1ef8fd3da6e2e7a635ae95604b47946946c1
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2018-03-10 15:20:08 +0400

    update

--- consul/bootstrap.sh
+++ consul/bootstrap.sh
@@ -2,7 +2,7 @@
 
 ## Script for Bootstraping Consul Cluster
 
-scriptVersion=1.0
+scriptVersion=1.1
 scriptName="Consul Master Bootstrap script"
 echo "*** You are Running $scriptName, Version : $scriptVersion ***"
 
@@ -19,22 +19,26 @@ chcon -Rt svirt_sandbox_file_t /opt/consul/data
 
 ## Find Default private ip address of server
 
-export default_iface=$(route | grep default | awk '{print $(NF)}')
-export defailt_ip=$(ip addr show dev "${default_iface}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
+ip a
+
+#export default_iface=$(route | grep default | awk '{print $(NF)}')
+read -e -p "Enter the interface name which you want consul to setup on " default_iface
+export default_iface
+export default_ip=$(ip addr show dev "${default_iface}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 export docker_ip=$(ip addr show dev "docker0" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 
 
 docker run -d -h master --restart=always -v /opt/consul/data:/data:Z \
-    -p ${defailt_ip}:8300:8300 \
-    -p ${defailt_ip}:8301:8301 \
-    -p ${defailt_ip}:8301:8301/udp \
-    -p ${defailt_ip}:8302:8302 \
-    -p ${defailt_ip}:8302:8302/udp \
-    -p ${defailt_ip}:8400:8400 \
-    -p ${defailt_ip}:8500:8500 \
+    -p ${default_ip}:8300:8300 \
+    -p ${default_ip}:8301:8301 \
+    -p ${default_ip}:8301:8301/udp \
+    -p ${default_ip}:8302:8302 \
+    -p ${default_ip}:8302:8302/udp \
+    -p ${default_ip}:8400:8400 \
+    -p ${default_ip}:8500:8500 \
     -p ${docker_ip}:53:53/udp \
-    progrium/consul -server -advertise ${defailt_ip} -bootstrap-expect ${count}
+    progrium/consul -server -advertise ${default_ip} -bootstrap-expect ${count}
 
-echo "Master IP is : ${defailt_ip}"
+echo "Master IP is : ${default_ip}"
 echo "Cluster Size is ${count}"
 echo "Use this ip for Nodes"
--- consul/node.sh
+++ consul/node.sh
@@ -4,7 +4,7 @@
 
 # Input Consul Cluster Size
 
-scriptVersion=1.0
+scriptVersion=1.1
 scriptName="Consul Node Bootstrap script"
 echo "*** You are Running $scriptName, Version : $scriptVersion ***"
 
@@ -21,20 +21,23 @@ mkdir -p /opt/consul/data
 chcon -Rt svirt_sandbox_file_t /opt/consul/data
 
 ## Find Default private ip address of server
+ip a
 
-export default_iface=$(route | grep default | awk '{print $(NF)}')
-export defailt_ip=$(ip addr show dev "${default_iface}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
+#export default_iface=$(route | grep default | awk '{print $(NF)}')
+read -e -p "Enter the interface name which you want consul to setup on " default_iface
+
+export default_ip=$(ip addr show dev "${default_iface}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 export docker_ip=$(ip addr show dev "docker0" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 
 docker run -d -h ${node_name} --restart=always -v /opt/consul/data:/data:Z \
-    -p ${defailt_ip}:8300:8300 \
-    -p ${defailt_ip}:8301:8301 \
-    -p ${defailt_ip}:8301:8301/udp \
-    -p ${defailt_ip}:8302:8302 \
-    -p ${defailt_ip}:8302:8302/udp \
-    -p ${defailt_ip}:8400:8400 \
-    -p ${defailt_ip}:8500:8500 \
+    -p ${default_ip}:8300:8300 \
+    -p ${default_ip}:8301:8301 \
+    -p ${default_ip}:8301:8301/udp \
+    -p ${default_ip}:8302:8302 \
+    -p ${default_ip}:8302:8302/udp \
+    -p ${default_ip}:8400:8400 \
+    -p ${default_ip}:8500:8500 \
     -p ${docker_ip}:53:53/udp \
-    progrium/consul -server -advertise ${defailt_ip} -join ${master_ip}
+    progrium/consul -server -advertise ${default_ip} -join ${master_ip}
 
 echo "Master IP is : ${master_ip}"
