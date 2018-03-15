d64ca09addbd069b744bb7a8bff594cf85cd8286
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2018-03-15 20:38:02 +0400

    overal aded checks

--- kubernetes/envvars.sh
+++ kubernetes/envvars.sh
@@ -5,6 +5,6 @@ export BLOCK_DEVICE="/dev/sdb"
 export KUBE_VERSION=""
 export KUBE_ADVERTISE_IP=""
 export KUBE_ADVERTISE_NAME="foo.bar.com"
-export NAMESPACE=("demo" "prod")
+export NAMESPACE=("demo")
 export TIMEZONE="Asia/Yerevan"
 export DEFAULT_NIC="eth0"
