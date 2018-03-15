410532cd6c6c6e6630819eea5134970441817f52
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2018-03-15 20:43:34 +0400

    un-needed questions

--- consul/bootstrap.sh
+++ consul/bootstrap.sh
@@ -17,13 +17,7 @@ rm -rf /opt/consul/data
 mkdir -p /opt/consul/data
 chcon -Rt svirt_sandbox_file_t /opt/consul/data
 
-## Find Default private ip address of server
-
-ip a
-
-#export default_iface=$(route | grep default | awk '{print $(NF)}')
-read -e -p "Enter the interface name which you want consul to setup on " default_iface
-export default_iface
+export default_iface=$(route | grep default | awk '{print $(NF)}')
 export default_ip=$(ip addr show dev "${default_iface}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 export docker_ip=$(ip addr show dev "docker0" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')
 
